709ee490741ae5a0884993d315b2ded3b0b3b10f
rcmorano <rcmorano@7a7f4aa0-ec84-47cc-a100-2f2ca1a7a0cd>
2010-04-28 10:54:45 +0000

    updated script

--- gensys/gensys-misc-scripts/update-upstream.sh
+++ gensys/gensys-misc-scripts/update-upstream.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 # 
 # Updates upstream release for a debian package
 #
@@ -11,4 +11,19 @@
 # option) any later version.  See http://www.gnu.org/copyleft/gpl.html for
 # the full text of the license.
 
-uscan && uupdate $(ls -rthal ../*tar.gz|grep -v orig|awk '{for (i=1;i<=NF;i++)  if ( $i ~ "gz" ) print $i}')
+
+uscan --verbose
+TGZ=$(ls -rthal ../*tar.gz|grep -v orig|awk '{for (i=1;i<=NF;i++)  if ( $i ~ "gz" ) print $i}')
+test -f $TGZ || exit 0
+TGZ_ORIG=$(echo $TGZ | sed 's/-\([0-9].*\)tar.gz/_\1orig.tar.gz/g')
+
+# cleaning
+test -h "$TGZ_ORIG" && rm -f "$TGZ_ORIG"
+test -d ${TGZ%.tar.gz} && rm -rf ${TGZ%.tar.gz}
+test -d ${TGZ_ORIG%.tar.gz} && rm -rf ${TGZ_ORIG%.tar.gz}
+
+
+cp $TGZ $TGZ_ORIG
+tar zvxf $TGZ_ORIG -C ../
+cp -a ${TGZ%.tar.gz} ${TGZ_ORIG%.tar.gz}
+exit 0
