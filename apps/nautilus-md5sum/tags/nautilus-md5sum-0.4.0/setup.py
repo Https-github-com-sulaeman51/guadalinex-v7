3ccbfe7241159582b6791729e1264c07642fd9c5
amian <amian@7a7f4aa0-ec84-47cc-a100-2f2ca1a7a0cd>
2010-04-28 09:32:34 +0000

    Updated to trunk branch format (only upstream code)

--- apps/nautilus-md5sum/tags/nautilus-md5sum-0.4.0/setup.py
+++ apps/nautilus-md5sum/tags/nautilus-md5sum-0.4.0/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from DistUtilsExtra.command import *
 
 setup(name='nautilus-md5sum',
-    version='0.3.5',
+    version='0.4.0',
     author='David Amian',
     author_email='amialinux@gmail.com',
     data_files=[('lib/nautilus/extensions-2.0/python/', ['scripts/nautilus-md5sum.py']),
