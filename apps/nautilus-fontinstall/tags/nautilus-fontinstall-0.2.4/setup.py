66ab729cb87ab66b95fd07ddeb41ad60a8dea086
amian <amian@7a7f4aa0-ec84-47cc-a100-2f2ca1a7a0cd>
2010-04-28 11:23:12 +0000

    Changed package structure and added fontinstall-ico

--- apps/nautilus-fontinstall/tags/nautilus-fontinstall-0.2.4/setup.py
+++ apps/nautilus-fontinstall/tags/nautilus-fontinstall-0.2.4/setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 from DistUtilsExtra.command import *
 
 setup(name='nautilus-fontinstall',
-    version='0.2.2-0guada1',
+    version='0.2.3',
     author='David Amian',
     author_email='amialinux@gmail.com',
     packages=['nautilus-fontinstall'],
-    data_files=[('lib/nautilus/extensions-2.0/python/', ['nautilus-fontinstall/nautilus-fontinstall.py']),
-                ('share/nautilus-fontinstall/',['nautilus-fontinstall/nautilus-fontinstall-notification', 'nautilus-fontinstall/fontinstall.glade'])],
+    data_files=[('lib/nautilus/extensions-2.0/python/', ['scripts/nautilus-fontinstall.py']),
+                ('share/nautilus-fontinstall/',['data/nautilus-fontinstall-notification', 'data/fontinstall.glade']),
+                ('share/icons/',['data/fontinstall-ico.png'])],
     cmdclass = { "build" : build_extra.build_extra,
         "build_i18n" :  build_i18n.build_i18n,
         "clean": clean_i18n.clean_i18n, 
