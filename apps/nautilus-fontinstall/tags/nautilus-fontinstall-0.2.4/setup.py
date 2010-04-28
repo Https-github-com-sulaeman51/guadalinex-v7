9b1f19d84d1b6035890fd1923e06812ba65b9874
amian <amian@7a7f4aa0-ec84-47cc-a100-2f2ca1a7a0cd>
2010-04-28 11:23:10 +0000

    First nautilus' extension's version to let the user install font files

---
+++ apps/nautilus-fontinstall/tags/nautilus-fontinstall-0.2.4/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from DistUtilsExtra.command import *
+
+setup(name='nautilus-fontinstall',
+    version='0.1.0',
+    author='David Amian',
+    author_email='amialinux@gmail.com',
+    packages=['nautilus-fontinstall'],
+    data_files=[('lib/nautilus/extensions-2.0/python/', ['nautilus-fontinstall/nautilus-fontinstall.py', 'nautilus-fontinstall/fontinstall.glade'])],
+    cmdclass = { "build" : build_extra.build_extra,
+        "build_i18n" :  build_i18n.build_i18n,
+        "clean": clean_i18n.clean_i18n, 
+        }
+    )
+
