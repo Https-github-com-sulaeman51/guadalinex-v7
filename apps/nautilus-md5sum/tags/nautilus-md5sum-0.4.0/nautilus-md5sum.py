2260bbb9f2293908dd6a407070800f3f03aff839
amian <amian@7a7f4aa0-ec84-47cc-a100-2f2ca1a7a0cd>
2010-04-28 09:32:25 +0000

    Added license and copyright to the code

--- apps/nautilus-md5sum/tags/nautilus-md5sum-0.4.0/nautilus-md5sum.py
+++ apps/nautilus-md5sum/tags/nautilus-md5sum-0.4.0/nautilus-md5sum.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-
-#David Amian y Alvaro Pinel
-#david.amian@price-roch.es
-#alvaro.pinel@price-roch.es
+#
+# Copyright (c) 2009, Junta de Andalucía <packmaster@guadalinex.org>
+#
+# This package is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this package; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Authors : David Amian <david.amian@price-roch.es>
+#           Alvaro Pinel <alvaro.pinel@price-roch.es>
 
 import urllib
 import nautilus
