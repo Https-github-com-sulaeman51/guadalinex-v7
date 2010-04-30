c77f45874568de5569acb24d1e63627a490d91e7
amian <amian@7a7f4aa0-ec84-47cc-a100-2f2ca1a7a0cd>
2010-04-30 09:48:27 +0000

    Updated to trunk branch version (only upstream code)

---
+++ apps/getapixel/tags/getapixel-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+from DistUtilsExtra.command import *
+
+setup(name='getapixel',
+    version='0.0.3',
+    author='David Amian',
+    author_email='amialinux@gmail.com',
+    data_files=[('share/getapixel/glade',['data/about.glade','data/createmosaic.glade',
+                'data/mosaic.glade', 'data/process.glade',
+                'data/selectpaths.glade']),
+                ('share/getapixel/', ['src/about.py',
+                'src/createmosaic.py','src/mosaic.py','src/process.py',
+                'src/selectpath.py','src/sendfile.py']),
+                ('share/getapixel/images/',['images/bggetapixel.png']),
+                ('share/icons/',['images/getapixel_46x46.png']),
+                ('bin/',['src/getapixel']),
+                ('share/applications/',['data/getapixel.desktop'])],
+    cmdclass = { "build" : build_extra.build_extra,
+        "build_i18n" :  build_i18n.build_i18n,
+        "clean": clean_i18n.clean_i18n,
+        }
+    )
+                
