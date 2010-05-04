3bb10b2b7e912a94abac122a16a2f1ac9ee906ef
stewie <stewie@7a7f4aa0-ec84-47cc-a100-2f2ca1a7a0cd>
2010-05-04 08:37:39 +0000

    Initil release in python

---
+++ apps/diagnostic-report/tags/diagnostic-report-1.2/scripts/diagnostic_report.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+#       diagnosis_report.py
+#       
+#       Copyright 2010 Álvaro Pinel Bueno <alvaropinel@gmail.com>
+#           date: 27/Apr/2010
+#
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+#
+#               ####################
+
+import pygtk
+pygtk.require("2.0")
+import gtk, os, tarfile, gettext
+
+gettext.install("diagnostic_report")
+
+class diagnosis:
+#Output var to store diagnosis report
+    path_out = "/tmp/diagnosis_report.txt"
+
+    
+    def show_file(self, file_r):
+        inp = open (file_r, "r")
+        outp = open(self.path_out, "a")
+        outp.write("** "+file_r+"\n")
+       
+        for line in inp.readlines():
+            
+            outp.write (line)
+        
+        outp.write("*----------\n")
+        outp.write("\n")
+        outp.close()
+
+    def show_binary_exit(self, command):
+        outp = open(self.path_out, "a")
+        outp.write("** "+command+"\n")
+        
+        for line in os.popen(command).readlines():
+            outp.write (line)
+            
+        outp.write("*----------\n")
+        outp.write("\n")
+        outp.close()
+
+
+    def on_wddiagn_destroy(self, widget, data=None):
+        gtk.main_quit()
+
+    def on_bt_cancel_clicked(self, widget, data=None):
+
+        if (os.path.exists(self.path_out)):
+            os.remove(self.path_out)
+
+        self.on_wddiagn_destroy(self)
+
+    def on_create_bt_clicked(self, widget, data=None):
+        curdir=os.getenv('HOME')
+	file_tar=curdir+"/Escritorio/informe_de_diagnostico.bz2"
+        
+        tar = tarfile.open(file_tar,  "w:bz2")
+        #Using True allow to only package the file without dirs
+        tar.add(self.path_out, os.path.basename(self.path_out))
+        tar.close()
+        
+        self.on_wddiagn_destroy(self)
+
+
+    def __init__(self):
+        self.builder=gtk.Builder()
+
+        #Get glade
+        self.builder.add_from_file("/usr/share/diagnostic_report/diagnostic_report.glade")
+        self.builder.connect_signals(self)
+
+        #Initial autoconnetions
+        self.bt_cancel=self.builder.get_object("bt_cancel")
+        self.wddiagn=self.builder.get_object("wddiagn")
+        self.textinfo=self.builder.get_object("textinfo")
+        self.create_bt=self.builder.get_object("create_bt")
+	
+            
+        self.wddiagn.set_title(_("Diagnostic report generator"))
+        self.wddiagn.set_icon_from_file("/usr/share/icons/diagnostic-report.png")
+
+
+        #Remove tmp file if exists
+        if (os.path.exists(self.path_out)):
+            os.remove(self.path_out)
+
+
+        ## Different system files and commands
+        # system wide related info
+        self.show_file ("/etc/lsb-release")
+        self.show_binary_exit ("dmesg")
+
+        # users related info
+        self.show_binary_exit ("whoami")
+        self.show_binary_exit ("date")
+        self.show_binary_exit ("cut -d: -f 1,3,4 /etc/passwd")
+        self.show_file ("/etc/group")
+
+        # X related info
+        self.show_file ("/etc/X11/xorg.conf")
+        self.show_file ("/var/log/Xorg.0.log")
+#        self.show_binary_exit ("ddcprobe")
+
+        # disks related info
+        self.show_binary_exit ("mount")
+        self.show_binary_exit ("df -h")
+        self.show_binary_exit ("fdisk -l")
+        self.show_file ("/etc/fstab")
+        self.show_file ("/etc/mtab")
+
+        # devices related info
+        self.show_binary_exit ("ls -lR /dev")
+        self.show_binary_exit ("lsusb")
+        self.show_binary_exit ("lsusb -v")
+
+        # network related info
+        self.show_binary_exit ("/sbin/ifconfig")
+        self.show_binary_exit ("/sbin/route -n")
+        self.show_file ("/etc/network/interfaces")
+
+        # modules related info
+        self.show_file ("/etc/modules")
+        self.show_binary_exit ("/sbin/lsmod")
+
+        # grub related info
+        self.show_file ("/boot/grub/menu.lst")
+
+        # 'proc' related info
+#        self.show_file ("/proc/apm")
+        self.show_file ("/proc/cmdline")
+        self.show_file ("/proc/cpuinfo")
+        self.show_file ("/proc/crypto")
+        self.show_file ("/proc/devices")
+        self.show_file ("/proc/dma")
+        self.show_file ("/proc/execdomains")
+        self.show_file ("/proc/fb")
+        self.show_file ("/proc/filesystems")
+        self.show_file ("/proc/interrupts")
+        self.show_file ("/proc/iomem")
+        self.show_file ("/proc/ioports")
+        self.show_file ("/proc/loadavg")
+        self.show_file ("/proc/locks")
+        self.show_file ("/proc/meminfo")
+        self.show_file ("/proc/misc")
+        self.show_file ("/proc/modules")
+        self.show_file ("/proc/mounts")
+        self.show_file ("/proc/mtrr")
+        self.show_file ("/proc/partitions")
+        self.show_file ("/proc/bus/pci/devices")
+        self.show_file ("/proc/slabinfo")
+        self.show_file ("/proc/stat")
+        self.show_file ("/proc/swaps")
+        self.show_file ("/proc/uptime")
+        self.show_file ("/proc/version")
+
+        #/proc/{    apm,cmdline,cpuinfo,crypto,devices,dma,execdomains,fb,filesystems,interrupts,iomem,
+        #           ioports,loadavg,locks,meminfo,misc,modules,mounts,mtrr,partitions,bus/pci/devices,slabinfo,stat,swaps,uptime,version}
+
+        # suitable lspci info for 'discover'
+        self.show_binary_exit ("lspci -n")
+        self.show_binary_exit ("lspci | sort")
+        ## -- ##
+
+        ##Attach file to text
+        if os.path.isfile(self.path_out):
+            f=open(self.path_out, "r")
+            self.content=unicode(f.read(), "utf-8")
+            f.close
+
+        buffer=self.textinfo.get_buffer()
+        buffer.set_text(self.content)
+
+        self.wddiagn.show_all()
+
+    def main(self):
+        gtk.main()
+
+if __name__ == "__main__":
+    diagn_instance=diagnosis()
+    diagn_instance.main()
+
+
