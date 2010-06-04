d2aaef78278841aa2c9da913968b859d74c47465
rcmorano <rcmorano@7a7f4aa0-ec84-47cc-a100-2f2ca1a7a0cd>
2010-06-04 10:35:26 +0000

    added firefox and gnome uri hanlders

---
+++ apps/runurl/tags/runurl-0.1/data/firefox-prefs/runurl.js
@@ -0,0 +1,6 @@
+/*
+ Adds firefox uri handler
+*/
+pref("network.protocol-handler.external.run",true);
+pref("network.protocol-handler.warn-external.run", false);
+pref("network.protocol-handler.expose.run", false);
